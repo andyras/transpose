873c7b8bc6df59de333eb824bbbab0a1a87c631f
Andrew Rasmussen <andyras@gmail.com>
2013-04-03 13:52:35 -0500

    now works silently and makes output by default.

--- src/transpose.cpp
+++ src/transpose.cpp
@@ -3,6 +3,8 @@
 #include <fstream>
 #include <cstdlib>
 
+//#define DEBUG_TRANSPOSE
+
 /* Counts the number of lines in a file. */
 int countRows(const char * fileName) {
  int n = 0;	// number of lines
@@ -12,10 +14,13 @@ int countRows(const char * fileName) {
   n++;
  }
  if (n == 0 ) {
-  std::cerr << "WARNING [" << __FUNCTION__ << "]: input file " << fileName << " is empty.\n";
+  std::cerr << "ERROR [" << __FUNCTION__ << "]: input file " << fileName << " is empty or missing.\n";
+  exit(1);
  }
 
+#ifdef DEBUG_TRANSPOSE
  std::cout << n << " rows in input.\n";
+#endif
 
  return n;
 }
@@ -41,7 +46,9 @@ int countCols(const char * fileName) {
   exit(1);
  }
 
+#ifdef DEBUG_TRANSPOSE
  std::cout << n << " columns in input.\n";
+#endif
 
  return n;
 }
@@ -73,19 +80,42 @@ void transpose(const char * fileName) {
  }
  */
  // read input
+ int flag;
  FILE * inputFile;
  inputFile = fopen(fileName, "r");
+ char junk [1024];	// junk string for checking for extra cols
+ double djunk;		// double for catching junk
 
  // check for file exists
  if (inputFile != NULL) {
   // loop over rows
   for (int ii = 0; ii < rows; ii++) {
    // loop over columns
-   for (int jj = 0; jj < rows; jj++) {
-    fscanf(inputFile, "%lf", &mat[ii*cols + jj]);
-    std::cout << mat[ii*cols + jj];
+   for (int jj = 0; jj < cols; jj++) {
+    flag = fscanf(inputFile, "%lf", &mat[ii*cols + jj]);
+    if ((flag == 0) || (flag == EOF)) {
+     std::cerr << "\nERROR: not enough columns of input at row "
+               << (ii+1) << ", column " << (jj+1) << ".\n";
+     exit(1);
+    }
+#ifdef DEBUG_TRANSPOSE
+    if (jj == 0) {
+     std::cout << mat[ii*cols + jj];
+    }
+    else {
+     std::cout << " " << mat[ii*cols + jj];
+    }
+#endif
    }
+   // check for extra columns
+   fgets(junk, 1024, inputFile);
+   if (sscanf(junk, "%lf", &djunk) == 1) {
+    std::cerr << "\nERROR: found extra columns on row " << (ii+1) << ".\n";
+    exit(1);
+   }
+#ifdef DEBUG_TRANSPOSE
    std::cout << "\n";
+#endif
   }
  }
  else {
@@ -95,13 +125,61 @@ void transpose(const char * fileName) {
  
  fclose(inputFile);
 
+ // write output
+
+ // create string for output file name
+ std::string outputFile = fileName;
+ std::string outputExt("TR");	// extension for output file name
+
+ // find position of last period and insert extension
+ size_t pos = outputFile.rfind(".");
+ if (pos == std::string::npos) {
+  outputFile.append(outputExt.c_str());
+ }
+ else {
+  outputFile.insert(pos, outputExt.c_str());
+ }
+#ifdef DEBUG_TRANSPOSE
+ std::cout << "output file name is " << outputFile << ".\n";
+#endif
+
+ FILE * output;
+ output = fopen(outputFile.c_str(), "w");
+
+ // loop over rows (transpose of original columns)
+ for (int ii = 0; ii < cols; ii++) {
+  // loop over columns (transpose of original rows)
+  for (int jj = 0; jj < rows; jj++) {
+   if (jj == 0) {
+#ifdef DEBUG_TRANSPOSE
+    std::cout << mat[jj*cols + ii];
+#endif
+    fprintf(output, "%-.10g", mat[jj*cols + ii]);
+   }
+   else {
+#ifdef DEBUG_TRANSPOSE
+    std::cout << " " << mat[jj*cols + ii];
+#endif
+    fprintf(output, " %-.10g", mat[jj*cols + ii]);
+   }
+  }
+#ifdef DEBUG_TRANSPOSE
+  std::cout << "\n";
+#endif
+  fprintf(output, "\n");
+ }
+
+ fclose(output);
+ 
  return;
 }
 
 int main (int argc, char ** argv) {
  // get file name
  char * inputFile = argv[1];
- std::cout << "File name is " << inputFile << "\n";
+#ifdef DEBUG_TRANSPOSE
+ std::cout << "Input file name is " << inputFile << "\n";
+#endif
 
  if (inputFile == NULL) {
   std::cerr << "Bogus!  You did not provide a file name.\n";
